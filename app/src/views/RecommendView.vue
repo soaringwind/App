ea341232a50e2bac696dafbf3da86b6efc5cd759
soaringwind <3163053781@qq.com>
2023-01-12 15:10:33 +0800

    Update RecommendView.vue

--- app/src/views/RecommendView.vue
+++ app/src/views/RecommendView.vue
@@ -1,6 +1,7 @@
 <template>
     <p>推荐</p>
-    <PostView :title="'这是第一个帖子'" :content="'hello world'"></PostView>
+    <PostView :title="'这是第一个帖子'" :content="'hello world'" :itemId="1"></PostView>
+    <PostView :title="'这是第二个帖子'" :content="'hello world'" :itemId="2"></PostView>
 </template>
 
 <script>
