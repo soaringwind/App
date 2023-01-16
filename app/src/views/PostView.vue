7a8b281223b9a01c2a5547b35005da706ce75c62
soaringwind <3163053781@qq.com>
2023-01-16 15:59:37 +0800

    Update PostView.vue

--- app/src/views/PostView.vue
+++ app/src/views/PostView.vue
@@ -1,56 +1,30 @@
-<script setup>
-// https://juejin.cn/post/7027977252331585544
-import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
-
-// 编辑器实例，必须用 shallowRef，重要！
-const editorRef = shallowRef()
-
-// 内容 HTML
-const valueHtml = ref('<p>hello</p>')
-
-// 模拟 ajax 异步获取内容
-onMounted(() => {
-    setTimeout(() => {
-        valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
-    }, 1500)
-})
-
-// 编辑器配置
-const editorConfig = {
-    placeholder: '请输入内容...',
-    MENU_CONF: { /* 菜单配置，下文解释 */ }
-}
-
-const handleCreated = (editor) => {
-    editorRef.value = editor // 记录 editor 实例，重要！
-}
-
-// 组件销毁时，及时销毁编辑器
-onBeforeUnmount(() => {
-    const editor = editorRef.value
-    if (editor == null) return
-    editor.destroy()
-})
-</script>
-
 <template>
-    <div style="border: 1px solid #ccc">
-        <!-- 工具栏 -->
-        <Toolbar
-            :editor="editorRef"
-            :defaultConfig="toolbarConfig"
-            style="border-bottom: 1px solid #ccc"
-        />
-        <!-- 编辑器 -->
-        <Editor
-            v-model="valueHtml"
-            :defaultConfig="editorConfig"
-            style="height: 500px; overflow-y: hidden;"
-            @onCreated="handleCreated"
-        />
-    </div>
+    <EditorCpe ref="child"></EditorCpe>
+    <router-link to="#">
+        <el-button @click="onEditorChange" class="sendText" style="background: orange; border: 0px">发出</el-button>
+    </router-link>
 </template>
 
-<!-- 别忘了引入样式 -->
-<style src="@wangeditor/editor/dist/css/style.css"></style>
+
+<script>
+import EditorCpe from '@/components/EditorCpe.vue';
+import { ref } from 'vue';
+
+export default {
+    name: 'PostView',
+    components: {
+        EditorCpe,
+    },
+    setup() {
+        const child = ref(null)
+        let onEditorChange = () => {
+            console.log("父组件收到的内容: ")
+            console.log(child.value.valueHtml)
+        }
+        return {
+            child, 
+            onEditorChange, 
+        }
+    }
+}
+</script> 
